4435495a479d3856f8e71f38330a0081f5b5133e
Cesar Salazar <cesarsrx@gmail.com>
2023-09-12 08:35:08 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from configurations import Configuration
 from configurations import values
+from datetime import timedelta
 
 
 
@@ -220,7 +221,8 @@ class Dev(Configuration):
           "rest_framework.authentication.BasicAuthentication",
           "rest_framework.authentication.SessionAuthentication",
           "rest_framework.authentication.TokenAuthentication",
-      ],
+          "rest_framework_simplejwt.authentication.JWTAuthentication"
+      ],    
       "DEFAULT_PERMISSION_CLASSES": [
           "rest_framework.permissions.IsAuthenticatedOrReadOnly"
       ],
@@ -246,6 +248,11 @@ class Dev(Configuration):
           "rest_framework.filters.OrderingFilter"
       ],
   }
+  SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+  }
+
 
 
 class Prod(Dev):
